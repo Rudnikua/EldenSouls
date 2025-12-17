4a3af092a94f7e82e9a0f5672b1cfb78aad0a724
Grompesto <lordgromp0806@gmail.com>
2025-12-17 17:41:52 +0100

    Music Zones
    Added Music Zones for locations soundtracks

---
+++ SoulsLike/Assets/Scripts/Sound/MusicZone.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MusicZone : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Stop();
+        }
+    }
+}
