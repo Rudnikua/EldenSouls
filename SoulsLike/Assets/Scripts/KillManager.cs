37803874fd4be3d36e51483bc7366e982e7ac787
Rudnikua <myroslavrudnikua@gmail.com>
2025-12-18 12:16:30 +0100

    Final fixes before presentation

--- SoulsLike/Assets/Scripts/KillManager.cs
+++ SoulsLike/Assets/Scripts/KillManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Mono.Cecil.Cil;
 using Unity.VisualScripting;
 
 public class KillManager : MonoBehaviour
